6ca44df9e8b3a58383d0d1eed99a55954c56389a
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-15 15:51:24 +0000

    finished the web page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: David.E
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a trapezoid.
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const baseA = parseInt(document.getElementById('base-a').value)
+  const baseB = parseInt(document.getElementById('base-b').value)
+  const heightOfTrapizoid = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTrapezoid = (baseA + baseB) * heightOfTrapizoid / 2
+
+  // output
+  document.getElementById("area").innerHTML = 'Area is: ' + areaOfTrapezoid + ' mm²'
+}
